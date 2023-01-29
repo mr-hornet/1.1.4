0ee76e89f9f5f4ebd77e4e60736980ec1297161e
Alexandr <mr-hornet@mail.ru>
2023-01-29 18:29:45 +1000

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,9 +2,7 @@ package jm.task.core.jdbc;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 2;
-        int f = 4;
-        int g = 6;
+
         // реализуйте алгоритм здесь
     }
 }
